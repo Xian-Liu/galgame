6f27901090560d7011fec60898bac64fad9fb4e2
Xian-Liu <13146602793@163.com>
2019-12-12 21:31:00 +0800

    up

--- script/main.ts
+++ script/main.ts
@@ -13,6 +13,23 @@ class Dialog {
     nextId;
     // 对话的对应立绘
     picture;
+    // type: 
+    // "text" 对话
+    // "option" 选项
+    type:string;
+    // 分支对应的下一个DIALOG ID
+    branches:Array<GalOption>;
+}
+
+class GalOption {
+    constructor(content, nextId) {
+        this.content = content
+        this.nextId = nextId
+    }
+    // 选项的文字
+    content: string;
+    // 选项对应的下一个 id
+    nextId: string;
 }
 
 // 所有的对话数据
@@ -20,25 +37,22 @@ class Dialog {
 
 
 // 创建一个 Map 用来储存下面的键值对
-const data = new Map();
+const data = new Map<string, Dialog>();
 
 // 给 Dialog实例的属性赋值
 (() => {
     let a = new Dialog();
     a.id = '0'
     a.content = "hello world 0"
-    a.nextId = '3'
+    a.nextId = '1'
+    a.type = "text"
     data.set(a.id, a)
-    a = new Dialog();
-    a.id = '3'
-    a.content = "hello world 1"
-    a.nextId = '4'
-    data.set(a.id, a)
-    a = new Dialog();
-    a.id = '4'
-    a.content = "hello world 2"
-    a.nextId = '0'
-    data.set(a.id, a)
+    a.id = '1'
+    a.type = "option"
+    a.branches = new Array<GalOption>()
+    a.branches.push(new GalOption("选2",'2'))
+    a.branches.push(new GalOption("选3", '3'))
+    data.set(a.id, a);
 })();
 
 // 下一步按钮的 DOM 对象
@@ -48,7 +62,18 @@ const dialog = $("#text");
 //  修改文本框的内容
 // 为 nextButton 的 click 事件添加一个监听器
 nextButton.click((event) => {
+    //将下一个 id 传递给 currentDialog
     let currentDialog = data.get(nextDialogId);
-    dialog.text(currentDialog.content);
-    nextDialogId = currentDialog.nextId;
+    //如果 other 属性字符串为 yes ,插入button按钮
+    switch(currentDialog.type) {
+        case "text":
+            dialog.text(currentDialog.content)
+            nextDialogId = currentDialog.nextId
+            break;
+        case "option":
+            
+            break;
+        default:
+            console.log("incorrect dialog type")
+    }
 });
